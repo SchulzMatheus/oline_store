89611461572e39c5f135aa130c2cb1c7006931b9
rosamenezes <g.rosamenezes1@gmail.com>
2022-11-09 15:14:58 -0300

    REQ 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
-export async function getCategories() {
-  // Implemente aqui
+
+export async function getCategories() { 
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-}
+export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  return response.json();
+} 
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
